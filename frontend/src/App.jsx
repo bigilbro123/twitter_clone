9397f046e0a73834fd42950e92d49e444f49d68a
Jishnu M <mjihnu990@gmail.com>
2024-08-13 00:36:22 +0530

    UI ready

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,29 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './page/home/HomePage';
+import SingUpPage from './page/auth/singup/SingUpPage.jsx';
+import LoginPage from './page/auth/login/LoginPage';
+import Sidebar from './components/common/Sidebar.jsx'
+import RightPanel from "./components/common/RightPanel.jsx";
+import Notification from "./page/notification/NotificationPage.jsx"
+import ProfilePage from "./page/profile/ProfilePage.jsx"
+const App = () => {
+    return (
+        <div className='flex max-w-6xl mx-auto'>
+            <Sidebar />
+            <Routes>
+                <Route path='/' element={<HomePage />} />
+                <Route path='/signup' element={<SingUpPage />} />
+                <Route path='/login' element={<LoginPage />} />
+                <Route path='/notifications' element={<Notification />} />
+                <Route path='/profile/:username' element={<ProfilePage />} />
 
-function App() {
-  return (
-    <div><div className="card bg-base-200 w-80">
-      <div className="card-body">
-        <input placeholder="Email" className="input input-bordered" />
-        <label className="label cursor-pointer">
-          Accept terms of use
-          <input type="checkbox" className="toggle" />
-        </label>
-        <label className="label cursor-pointer">
-          Submit to newsletter
-          <input type="checkbox" className="toggle" />
-        </label>
-        <button className="btn btn-neutral">Save</button>
-      </div>
-    </div></div>
-  )
+            </Routes>
+
+            <RightPanel />
+
+        </div>
+    )
 }
 
 export default App
